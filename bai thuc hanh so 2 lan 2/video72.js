6d90a956f0da0171e7b9249106c27a7b30e11d57
luyn1610 <102026801+luyn1610@users.noreply.github.com>
2025-12-18 03:29:46 +0700

    lam lai bai thuc hanh 2
    dua duoc noi dung api len man hinh

--- bai thuc hanh so 2 lan 2/video72.js
+++ bai thuc hanh so 2 lan 2/video72.js
@@ -1,1 +1,26 @@
+//lay du lieu api
+const getApi = async () => {
+    const res = await fetch("http://localhost:8000/blogs");
+    const data = await res.json();
+    console.log(data);
+    return data;
+};
 
+//cho du lieu api len man hinh
+const handleAddBlog = async () => {
+    data = await getApi();
+
+    const body = document.querySelector("#blogs-body");
+    data.forEach((blog) => {
+        body.innerHTML += `
+         <td>${blog.id}</td>
+         <td>${blog.title}</td>
+         <td>${blog.author}</td>
+         <td>${blog.content}</td>
+         <td><button>xoa</button></td>
+      `;
+    });
+};
+handleAddBlog();
+//them du lieu vao api va man hinh
+//xoa du lieu api va man hinh
